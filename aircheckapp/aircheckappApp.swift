0b407101f3b95bcc7cc1cddfb988682b53806965
adamstefanik <adamm.stefanik11@gmail.com>
2026-04-13 20:33:26 +0200

    Initial Commit

---
+++ aircheckapp/aircheckappApp.swift
@@ -0,0 +1,17 @@
+//
+//  aircheckappApp.swift
+//  aircheckapp
+//
+//  Created by Adam S. Štefánik on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct aircheckappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
