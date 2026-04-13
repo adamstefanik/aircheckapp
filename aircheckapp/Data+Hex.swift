f3e2a6282bdf3ce9e6a20eddcfcc13de7daa77b4
adamstefanik <adamm.stefanik11@gmail.com>
2026-04-13 20:33:26 +0200

    feat: add Data+Hex extension for miIO token handling

---
+++ aircheckapp/Data+Hex.swift
@@ -0,0 +1,22 @@
+// aircheckapp/Data+Hex.swift
+import Foundation
+
+extension Data {
+    init?(hexString: String) {
+        let hex = hexString.lowercased()
+        guard hex.count % 2 == 0 else { return nil }
+        var data = Data(capacity: hex.count / 2)
+        var index = hex.startIndex
+        while index < hex.endIndex {
+            let next = hex.index(index, offsetBy: 2)
+            guard let byte = UInt8(hex[index..<next], radix: 16) else { return nil }
+            data.append(byte)
+            index = next
+        }
+        self = data
+    }
+
+    var hexString: String {
+        map { String(format: "%02x", $0) }.joined()
+    }
+}
---
+++ aircheckappTests/DataHexTests.swift
@@ -0,0 +1,30 @@
+// aircheckappTests/DataHexTests.swift
+import XCTest
+@testable import aircheckapp
+
+final class DataHexTests: XCTestCase {
+    func test_hexToData_validToken() {
+        let data = Data(hexString: "00112233445566778899aabbccddeeff")
+        XCTAssertEqual(data?.count, 16)
+        XCTAssertEqual(data?[0], 0x00)
+        XCTAssertEqual(data?[15], 0xFF)
+    }
+
+    func test_hexToData_uppercase() {
+        let data = Data(hexString: "AABBCCDD")
+        XCTAssertEqual(data?.count, 4)
+        XCTAssertEqual(data?[0], 0xAA)
+    }
+
+    func test_hexToData_oddLength_returnsNil() {
+        XCTAssertNil(Data(hexString: "abc"))
+    }
+
+    func test_hexToData_invalidChars_returnsNil() {
+        XCTAssertNil(Data(hexString: "ZZZZ"))
+    }
+
+    func test_dataToHex() {
+        XCTAssertEqual(Data([0xDE, 0xAD, 0xBE, 0xEF]).hexString, "deadbeef")
+    }
+}
